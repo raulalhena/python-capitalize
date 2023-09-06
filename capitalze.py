cf59a32ab59c30798a3bb097e2a5d559c17b7083
Raul <raul.alhena@gmail.com>
2023-09-06 10:02:03 +0200

    refactored

--- capitalze.py
+++ capitalze.py
@@ -1,10 +1,11 @@
 # Función para capitalizar un string. Es decir, en mayúsculas la primera 
 # letra de cada palabra del array
 
-words = ['hola', 'estoy', 'es', 'tu', 'curso', 'de', 'pitón', 'en', 'español! :D']
+words = 'hola estoy es tu curso de pitón en español! :D'
 
 def capitlize (words):
-    for word in words:
+    arr = words.split(' ')
+    for word in arr:
         print(word.capitalize(), end= ' ')
 
 capitlize(words)
